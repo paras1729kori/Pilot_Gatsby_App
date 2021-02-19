428c774a5075893ced53ad531f42c51cb17bf188
paras1729kori <paras1799kori@gmail.com>
2021-02-19 12:32:39 +0530

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,12 +5,27 @@ import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
+// Default Code
+// const IndexPage = () => (
+//   <Layout>
+//     <SEO title="Home" />
+//     <h1>Hi people</h1>
+//     <p>Welcome to your new Gatsby site.</p>
+//     <p>Now go build something great.</p>
+//     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+//       <Image />
+//     </div>
+//     <Link to="/page-2/">Go to page 2</Link> <br />
+//     <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+//   </Layout>
+// )
+
+// My Code
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <SEO title="Pilot" />
+    <h1>Learn to design & code React App</h1>
+    <p>Complete courses about the best tools and design systems. Prototype and build apps with React and Swift.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
